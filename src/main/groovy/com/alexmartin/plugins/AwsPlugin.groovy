3efb4ebb1a2d76e1459dc75768b1d9d816ddaff0
Alex Martin <alexander.martin@excella.com>
2019-01-24 00:13:17 -0500

    Implement create stack with extension

--- src/main/groovy/com/alexmartin/plugins/AwsPlugin.groovy
+++ src/main/groovy/com/alexmartin/plugins/AwsPlugin.groovy
@@ -1,6 +1,7 @@
 package com.alexmartin.plugins;
 
-import com.alexmartin.extensions.CreateStackExtension;
+import com.alexmartin.extensions.CreateStackExtension
+import com.alexmartin.tasks.CreateStackTask;
 import com.alexmartin.tasks.ListStacksTask;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
@@ -10,10 +11,11 @@ public class AwsPlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
-        def extension = project.getExtensions().create("createStack", CreateStackExtension, project)
-        project.getTasks().create("listStacks", ListStacksTask) {
+        def extension = project.extensions.create("createStack", CreateStackExtension, project)
+        project.tasks.create("createStack", CreateStackTask) {
             stackName = extension.stackName
             filePath = extension.filePath
         }
+        project.tasks.create("listStacks", ListStacksTask)
     }
 }
--- src/main/java/com/alexmartin/tasks/CreateStackTask.java
+++ src/main/java/com/alexmartin/tasks/CreateStackTask.java
@@ -2,16 +2,45 @@ package com.alexmartin.tasks;
 
 import com.amazonaws.services.cloudformation.AmazonCloudFormation;
 import com.amazonaws.services.cloudformation.AmazonCloudFormationClientBuilder;
+import com.amazonaws.services.cloudformation.model.CreateStackRequest;
 import org.gradle.api.DefaultTask;
+import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.TaskAction;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class CreateStackTask extends DefaultTask {
 
     private AmazonCloudFormation client = AmazonCloudFormationClientBuilder.standard().build();
 
+    final Property<String> stackName = getProject().getObjects().property(String.class);
+    final Property<String> filePath = getProject().getObjects().property(String.class);
+
     @TaskAction
     public void createStack() {
+        CreateStackRequest stackRequest = new CreateStackRequest();
+        try {
+            stackRequest.withStackName(stackName.get());
+            stackRequest.withTemplateBody(
+                    convertStreamToString(
+                            CreateStackTask.class.getResourceAsStream(filePath.get())));
+            client.createStack(stackRequest);
+        } catch (Exception e) {
+            System.out.println("Error creating stack" + e.getMessage());
+        }
+    }
 
+    private static String convertStreamToString(InputStream in) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        StringBuilder stringbuilder = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            stringbuilder.append(line + "\n");
+        }
+        in.close();
+        return stringbuilder.toString();
     }
 
 }
