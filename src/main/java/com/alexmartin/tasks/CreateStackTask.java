8e8ade6160b5f01b59062e7b4f6d70b4d94d3951
Alex Martin <alexander.martin@excella.com>
2019-01-29 20:12:41 -0500

    Refactor CreateStackTask to wait for successful creation before returning success.

--- src/main/java/com/alexmartin/tasks/CreateStackTask.java
+++ src/main/java/com/alexmartin/tasks/CreateStackTask.java
@@ -2,8 +2,7 @@ package com.alexmartin.tasks;
 
 import com.amazonaws.services.cloudformation.AmazonCloudFormation;
 import com.amazonaws.services.cloudformation.AmazonCloudFormationClientBuilder;
-import com.amazonaws.services.cloudformation.model.CreateStackRequest;
-import com.amazonaws.services.cloudformation.model.Parameter;
+import com.amazonaws.services.cloudformation.model.*;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.provider.MapProperty;
 import org.gradle.api.provider.Property;
@@ -61,8 +60,9 @@ public class CreateStackTask extends DefaultTask {
             stackRequest.withStackName(stackName);
             stackRequest.withTemplateBody(new String(Files.readAllBytes(Paths.get(filePath))));
             stackRequest.withParameters(convertMapToParams());
-            System.out.println(stackRequest.toString());
             client.createStack(stackRequest);
+            System.out.println(String.format("Creating Stack %s", stackName));
+            waitForStackCreationToComplete();
         } catch (Exception e) {
             System.out.println("Error creating stack: " + e.getMessage());
         }
@@ -77,5 +77,38 @@ public class CreateStackTask extends DefaultTask {
             cloudformationParameters.add(temp);
         }
         return cloudformationParameters;
+    }
+
+    private void waitForStackCreationToComplete() throws InterruptedException {
+        DescribeStacksRequest check = new DescribeStacksRequest().withStackName(getStackName());
+        boolean completed = false;
+        while(!completed) {
+            List<Stack> results = client.describeStacks(check).getStacks();
+            if(results.isEmpty()){
+                throw new RuntimeException("Stack Does Not Exist.");
+            } else {
+                if(results.stream().anyMatch(this::checkIfStackCreationFailed)) {
+                    throw new RuntimeException("Stack Creation or Update Failed");
+                } else if(results.stream().anyMatch(this::checkIfStackCreationSucceeded)) {
+                    System.out.println("Stack Creation Complete");
+                    completed = true;
+                }
+            }
+            if(!completed) Thread.sleep(5000);
+        }
+    }
+
+    private boolean checkIfStackCreationSucceeded(Stack stack) {
+        StackStatus status = StackStatus.fromValue(stack.getStackStatus());
+        return status.equals(StackStatus.CREATE_COMPLETE) ||
+                status.equals(StackStatus.UPDATE_COMPLETE);
+    }
+
+    private boolean checkIfStackCreationFailed(Stack stack) {
+        StackStatus status = StackStatus.fromValue(stack.getStackStatus());
+        return status.equals(StackStatus.CREATE_FAILED) ||
+                status.equals( StackStatus.ROLLBACK_FAILED) ||
+                status.equals(StackStatus.UPDATE_ROLLBACK_COMPLETE) ||
+                status.equals(StackStatus.UPDATE_ROLLBACK_FAILED);
     }
 }
